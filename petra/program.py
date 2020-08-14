514fdab13635afa13e51b73d223d45b6afb3e7a2
Rita Tlemcani <ritabt@sapling.stanford.edu>
2020-08-14 10:20:06 -0700

    add get_target_machine()

--- petra/program.py
+++ petra/program.py
@@ -25,7 +25,12 @@ class Program(object):
         self.module = ir.Module(name=name)
         self.functypes: Dict[str, Tuple[Ftypein, Ftypeout]] = dict()
         self.funcs: Dict[str, ir.Function] = dict()
-        self.target_machine: binding.TargetMachine = None
+        self.llvm_initialized = True
+        binding.initialize()
+        binding.initialize_native_target()
+        binding.initialize_native_asmprinter()
+        target = binding.Target.from_default_triple()  
+        self.target_machine: binding.TargetMachine = target.create_target_machine()
 
     def add_func_decl(
         self, 
@@ -80,37 +85,20 @@ class Program(object):
         return str(self.module)
 
     def save_object(self, filename: str) -> None:
-        if not self.llvm_initialized:
-            self.llvm_initialized = True
-            binding.initialize()
-            binding.initialize_native_target()
-            binding.initialize_native_asmprinter()
-        # FIXME: Not sure why MyPy can't type check this, maybe a bug
-        target = binding.Target.from_default_triple()  # type: ignore
-        target_machine = target.create_target_machine()
         backing_mod = binding.parse_assembly(self.to_llvm())
         with open(filename, "wb") as f:
-            f.write(target_machine.emit_object(backing_mod))
+            f.write(self.target_machine.emit_object(backing_mod))
 
     def compile(self) -> binding.ExecutionEngine:
-        if not self.llvm_initialized:
-            self.llvm_initialized = True
-            binding.initialize()
-            binding.initialize_native_target()
-            binding.initialize_native_asmprinter()
-        # FIXME: Not sure why MyPy can't type check this, maybe a bug
-        target = binding.Target.from_default_triple()  # type: ignore
-        target_machine = target.create_target_machine()
-        self.target_machine = target_machine
         print(self.to_llvm())
         backing_mod = binding.parse_assembly(self.to_llvm())
-        engine = binding.create_mcjit_compiler(backing_mod, target_machine)
+        engine = binding.create_mcjit_compiler(backing_mod, self.target_machine)
         engine.finalize_object()
         engine.run_static_constructors()
         return engine
 
-    def get_target_machine(self) -> binding.TargetMachine:
-        return self.target_machine
+    # def get_target_machine(self) -> binding.TargetMachine:
+    #     return self.target_machine
 
     def load_library(self, filename: str) -> None:
         binding.load_library_permanently(filename)
